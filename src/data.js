14246f872bf5bceec4ed02349e99d09e47fb4056
codeKAMAT <easykamat@gmail.com>
2021-11-24 15:06:09 +0530

    adding slider

---
+++ src/data.js
@@ -0,0 +1,24 @@
+
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/XS8DGmL/24646-6-vegetable-photos.png",
+      title: "Fresh and Healthy",
+      desc: "Vegetables",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/BLSPW0y/fruit2.png",
+      title: "Healthy Fresh Fruit",
+      desc: "With Best Quality",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/G5NTh0H/grocery3.png",
+      title: "Wide range of",
+      desc: "Grocery Items",
+      bg: "fbf0f4",
+    },
+  ];
